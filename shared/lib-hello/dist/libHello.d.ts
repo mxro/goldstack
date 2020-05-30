d9e94ff7e87df0223f2c4947e70ed4591a8caae1
Max Rohde <maxrohde.public@gmail.com>
2020-05-30 12:04:56 +1000

    Remove dist folder

--- shared/lib-hello/dist/libHello.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const message = "hello";
-//# sourceMappingURL=libHello.d.ts.map
--- shared/lib-hello/dist/libHello.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.message = void 0;
-exports.message = 'hello';
-//# sourceMappingURL=libHello.js.map
